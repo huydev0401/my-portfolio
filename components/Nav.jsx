808b9dcdaf4ba74535cad772e4aeb153ad9e45d1
huydev0401 <quanghuynguyennqh@gmail.com>
2024-05-19 15:41:02 +0700

    Add Header

---
+++ components/Nav.jsx
@@ -0,0 +1,39 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
+
+const links = [
+  { path: '/', name: 'home' },
+  { path: '/projects', name: 'my projects' },
+  { path: '/contact', name: 'contact' }
+]
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+  const path = usePathname()
+
+  return (
+    <div className={`${containerStyles}`}>
+      {links.map((link, index) => {
+        return <Link
+          className={`capitalize ${linkStyles}`}
+          key={index}
+          href={link.path}
+        >
+          {link.path === path && (
+            <motion.span
+              initial={{ y: '-100%' }}
+              animate={{ y: 0 }}
+              transition={{ type: 'tween' }}
+              layoutId='underline'
+              className={`${underlineStyles}`}
+            />
+          )}
+          {link.name}
+        </Link>
+      })}
+    </div>
+  )
+}
+
+export default Nav
