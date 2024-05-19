f0aa282b7319240c2b8215cd92f1992e21c401bc
huydev0401 <quanghuynguyennqh@gmail.com>
2024-05-19 13:45:06 +0700

    Add ThemeToggler

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import ThemeToggler from './ThemeToggler'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <ThemeToggler />
+    </header>
   )
 }
 
